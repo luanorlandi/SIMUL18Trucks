c2319153b2328ebb6e825b6d568a255e840b76ee
Luan Orlandi <orlandi.luan@gmail.com>
2016-05-21 13:34:40 -0300

    Add material to illumination class

--- src/main/java/game/Illumination.java
+++ src/main/java/game/Illumination.java
@@ -6,6 +6,7 @@
 package game;
 
 import br.usp.icmc.vicg.gl.core.Light;
+import br.usp.icmc.vicg.gl.core.Material;
 import br.usp.icmc.vicg.gl.util.Shader;
 import javax.media.opengl.GL3;
 
@@ -15,20 +16,36 @@ import javax.media.opengl.GL3;
  */
 public class Illumination {
     private final Light light;
+    private final Material material;
+    private float i;
     
     public Illumination() {
         light = new Light();
+        material = new Material();
+        
+        i = 0.0f;
     }
     
     public void init(GL3 gl, Shader shader) {
-        light.setPosition(new float[]{10, 10, 50, 1.0f});
+        light.setPosition(new float[]{30.0f, 5.0f, -5.0f, 1.0f});
         light.setAmbientColor(new float[]{0.1f, 0.1f, 0.1f, 1.0f});
-        light.setDiffuseColor(new float[]{0.75f, 0.75f, 0.75f, 1.0f});
-        light.setSpecularColor(new float[]{0.7f, 0.7f, 0.7f, 1.0f});
+        light.setDiffuseColor(new float[]{1.0f, 1.0f, 1.0f, 1.0f});
+        light.setSpecularColor(new float[]{1.0f, 1.0f, 1.0f, 1.0f});
+        
+        light.setConstantAttenuation(1.0f);
+        light.setLinearAttenuation(0.15f);
+        light.setQuadraticAttenuation(0.1f);
         light.init(gl, shader);
+        
+        material.init(gl, shader);
+        material.setAmbientColor(new float[]{0.1f, 0.1f, 0.1f, 1.0f});
+        material.setDiffuseColor(new float[]{1.0f, 1.0f, 0.0f, 1.0f});
+        material.setSpecularColor(new float[]{1.0f, 1.0f, 1.0f, 1.0f});
+        material.setSpecularExponent(64.0f);
     }
     
     public void bind() {
         light.bind();
+        material.bind();
     }
 }
